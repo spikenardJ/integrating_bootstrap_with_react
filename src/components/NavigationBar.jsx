35ac93686cb6977b379ce70af92df1ec6b5dd0ae
spikenardJ <jennifer_annette@hotmail.com>
2025-01-12 18:35:28 -0600

    Third commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
 import React from 'react';
+import { Navbar, Nav, NavbarToggle } from "react-bootstrap"
 
 function NavigationBar() {
     return (
-        <nav className="nav-bar">
-            <NavLink to="/" activeClassName="active">HomePage</NavLink>
-            <NavLink to="/products-page" activeClassName="active">Products Page</NavLink>
-        </nav>
+        <Navbar bg="light" expand="lg" >
+        <Navbar.Brand href="/" >Corner Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/" activeclassname="active">
+            HomePage
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products-page" activeclassname="active">
+            Products Page
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
 }
 
